53200e1b496cf663638a4c676d9d892ac2781c3d
axel.legue <axel.legue31@gmail.com>
2020-05-15 02:14:33 +0200

    feat: implement combineLatest from the 2 feeds

--- app/src/main/java/com/tehmou/book/androidnewsreaderexample/MainActivity.kt
+++ app/src/main/java/com/tehmou/book/androidnewsreaderexample/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.tehmou.book.androidnewsreaderexample
 
+
 import android.os.Bundle
-import android.util.Log
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.tehmou.book.androidnewsreaderexample.network.Entry
 import com.tehmou.book.androidnewsreaderexample.network.FeedObservable
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.BiFunction
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,19 +20,41 @@ class MainActivity : AppCompatActivity() {
 
         val googleFeedObservable = FeedObservable.getFeed(GOOGLE_NEWS)
         val registerFeedObservable = FeedObservable.getFeed(REGISTER_NEWS)
+        val combineLatestObservable: Observable<List<Entry>> =
+                Observable.combineLatest(
+                        googleFeedObservable,
+                        registerFeedObservable,
+                        BiFunction { googleList, registerList -> mergeList(googleList, registerList) }
+                )
 
-        googleFeedObservable.observeOn(AndroidSchedulers.mainThread())
+//        googleFeedObservable
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(this::drawList)
+//
+//        registerFeedObservable
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(this::drawList)
+
+
+        combineLatestObservable
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(this::drawList)
 
-        registerFeedObservable.observeOn(AndroidSchedulers.mainThread())
-                .subscribe(this::drawList)
 
     }
 
     private fun drawList(entries: List<Entry>) {
-        for (i in entries) {
-            Log.i("MainActivity", "$i \n")
-        }
+        val itemsAdapter: ArrayAdapter<Entry> = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                entries)
+        feedsList.adapter = itemsAdapter
+    }
+
+    private fun mergeList(entriesA: List<Entry>, entriesB: List<Entry>): List<Entry> {
+        val finalList = mutableListOf<Entry>()
+        finalList.addAll(entriesA)
+        finalList.addAll(entriesB)
+        return finalList
     }
 
 
